4a22da7ac43b1743d0ef485c0456bd25469a6755
Esat <3s4t47@gmail.com>
2026-05-23 16:19:52 +0200

    main.py: ilk çalışan sürüm eklendi

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Merhaba, ben KIZIL.")
+    print("Senin kişisel yapay zeka asistanınım. Henüz emekleme aşamasındayım ama hızla büyüyeceğim.")
+
+if __name__ == "__main__":
+    main()
